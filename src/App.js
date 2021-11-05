646ccea9acfbf95efc51c7cbf61a18fde1ed0914
Savannah <savmaxwell@gmail.com>
2021-11-05 12:10:34 -0600

    completed MVP

--- src/App.js
+++ src/App.js
@@ -1,17 +1,31 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 import './App.css';
+import axios from 'axios';
+import { BASE_URL } from './constants';
+import Character from './components/Character';
+import styled from 'styled-components';
 
 const App = () => {
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
-
+  const [characters, setCharacters] = useState([])
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
   // sync up with, if any.
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+    .then(res => {
+      setCharacters(res.data)
+    })
+    .catch(err => console.error(err))
+  }, [])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">Star War's Characters</h1>
+      { characters.map(char => {
+        return <Character info={char} key={char.name} />
+      })}
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,80 @@
 // Write your Character component here
+import React, { useState } from "react";
+import styled from "styled-components";
+
+const StyledCharacers = styled.div`
+
+    max-width: 100%;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+
+    .chars {
+        display: flex;
+        justify-content: center;
+        margin-bottom: 3%;
+    }
+
+    .char-info {
+        width: 50%;
+        background-image: url('https://images.unsplash.com/photo-1506318137071-a8e063b4bec0?ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&ixlib=rb-1.2.1&auto=format&fit=crop&w=2093&q=80');
+        color: yellow;
+    }
+
+    .name {
+ 
+        width: 50%;
+        display: flex;
+        justify-self: center;
+        align-self: center;
+        justify-content: center;
+        background-color: black;
+
+    h1 {
+        color: yellow;
+        background-color: black;
+        font-family: 'Londrina Outline', cursive;
+        font-size: 3rem;
+    }
+`
+
+const Character = (props) => {
+
+    const {characters, info} = props
+
+    return(
+        <StyledCharacers>
+            <div className="name" >
+                <h1>
+                    {info.name}
+                </h1>
+            </div>
+            <div className="chars" >
+                <div className="char-info" >
+                    <p>
+                        Birth Year: {info.birth_year}
+                    </p>
+                    <p>
+                        Eye Color: {info.eye_color}
+                    </p>
+                    <p>
+                        Hair Color: {info.hair_color}
+                    </p>
+                    <p>
+                        Height: {info.height}
+                    </p>
+                {/* <p>
+                    Homeworld: {info.homeworld}
+                </p> */}
+                </div>
+            </div>
+            {/* <div>
+                <h2>
+                    Films: {info.films}
+                </h2>
+            </div> */}
+        </StyledCharacers>
+    )
+}
+
+export default Character;
